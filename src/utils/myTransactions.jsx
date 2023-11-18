2e90ee5bdc9dd9fc6fc9fbe27e9c0eacafcd7e47
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-11-18 17:38:26 +0300

    arrange the order of transactions

--- src/utils/myTransactions.jsx
+++ src/utils/myTransactions.jsx
@@ -1,145 +1,146 @@
 export const myTransactions = [
-    {
-        id: 1,
-        description: "Groceries",
-        amount: -50.00,
-        date: "2023-11-01",
-        category: "Food",
-        type: "expense"
-    },
-    {
-        id: 2,
-        description: "Salary",
-        amount: 2000.00,
-        date: "2023-11-02",
-        category: "income",
-        type: "income"
-    },{
-        id: 22,
-        description: "Salary",
-        amount: 2000.00,
-        date: "2023-13-02",
-        category: "income",
-        type: "income"
-    },{
-        id: 42,
-        description: "Salary",
-        amount: 2000.00,
-        date: "2023-14-02",
-        category: "income",
-        type: "income"
-    },{
-        id: 82,
-        description: "Salary",
-        amount: 2000.00,
-        date: "2023-15-02",
-        category: "income",
-        type: "income"
-    },
-    {
-        id: 3,
-        description: "Gas",
-        amount: -30.00,
-        date: "2023-11-03",
-        category: "Transportation",
-        type: "expense"
-
-    },
-    {
-        id: 4,
-        description: "Netflix Subscription",
-        amount: -15.00,
-        date: "2023-11-04",
-        category: "Entertainment",
-        type: "expense"
-    },
-    {
-        id: 5,
-        description: "Electric Bill",
-        amount: -100.00,
-        date: "2023-11-05",
-        category: "Utilities",
-        type: "expense"
-
-    },
-    {
-        id: 6,
-        description: "Dinner with Friends",
-        amount: -75.00,
-        date: "2023-11-06",
-        category: "Food",
-        type: "expense"
-    },
-    {
-        id: 7,
-        description: "Freelance Work",
-        amount: 500.00,
-        date: "2023-11-07",
-        category: "income",
-        type: "income"
-    },
-    {
-        id: 8,
-        description: "New Phone",
-        amount: -800.00,
-        date: "2023-11-08",
-        category: "Shopping",
-        type: "expense"
-    },
-    {
-        id: 9,
-        description: "Gym Membership",
-        amount: -50.00,
-        date: "2023-11-09",
-        category: "Health",
-        type: "expense"
-    },
-    {
-        id: 10,
-        description: "Car Insurance",
-        amount: -150.00,
-        date: "2023-11-10",
-        category: "Transportation",
-        type: "expense"
-    },
-    {
-        id: 11,
-        description: "Birthday Gift",
-        amount: -50.00,
-        date: "2023-11-11",
-        category: "Gifts",
-        type: "expense"
-    },
-    {
-        id: 12,
-        description: "Freelance Work",
-        amount: 300.00,
-        date: "2023-11-12",
-        category: "income",
-        type: "income"
-    },
-    {
-        id: 13,
-        description: "Haircut",
-        amount: -25.00,
-        date: "2023-11-13",
-        category: "Personal Care",
-        type: "expense"
-    },
-    {
-        id: 14,
-        description: "Rent",
-        amount: -1000.00,
-        date: "2023-11-14",
-        category: "Housing",
-        type: "expense"
-    },
-    {
-        id: 15,
-        description: "New Laptop",
-        amount: -1200.00,
-        date: "2023-11-15",
-        category: "Shopping",
-        type: "expense"
-    }
-];
+  {
+    id: 1,
+    description: 'Groceries',
+    amount: -50.0,
+    date: '2023-11-01',
+    category: 'Food',
+    type: 'expense',
+  },
+  {
+    id: 2,
+    description: 'Salary',
+    amount: 2000.0,
+    date: '2023-11-02',
+    category: 'income',
+    type: 'income',
+  },
+  {
+    id: 3,
+    description: 'Gas',
+    amount: -30.0,
+    date: '2023-11-03',
+    category: 'Transportation',
+    type: 'expense',
+  },
+  {
+    id: 4,
+    description: 'Netflix Subscription',
+    amount: -15.0,
+    date: '2023-11-04',
+    category: 'Entertainment',
+    type: 'expense',
+  },
+  {
+    id: 5,
+    description: 'Electric Bill',
+    amount: -100.0,
+    date: '2023-11-05',
+    category: 'Utilities',
+    type: 'expense',
+  },
+  {
+    id: 6,
+    description: 'Dinner with Friends',
+    amount: -75.0,
+    date: '2023-11-06',
+    category: 'Food',
+    type: 'expense',
+  },
+  {
+    id: 7,
+    description: 'Freelance Work',
+    amount: 500.0,
+    date: '2023-11-07',
+    category: 'income',
+    type: 'income',
+  },
+  {
+    id: 8,
+    description: 'New Phone',
+    amount: -800.0,
+    date: '2023-11-08',
+    category: 'Shopping',
+    type: 'expense',
+  },
+  {
+    id: 9,
+    description: 'Gym Membership',
+    amount: -50.0,
+    date: '2023-11-09',
+    category: 'Health',
+    type: 'expense',
+  },
+  {
+    id: 10,
+    description: 'Car Insurance',
+    amount: -150.0,
+    date: '2023-11-10',
+    category: 'Transportation',
+    type: 'expense',
+  },
+  {
+    id: 11,
+    description: 'Birthday Gift',
+    amount: -50.0,
+    date: '2023-11-11',
+    category: 'Gifts',
+    type: 'expense',
+  },
+  {
+    id: 12,
+    description: 'Freelance Work',
+    amount: 300.0,
+    date: '2023-11-12',
+    category: 'income',
+    type: 'income',
+  },
+  {
+    id: 13,
+    description: 'Haircut',
+    amount: -25.0,
+    date: '2023-11-13',
+    category: 'Personal Care',
+    type: 'expense',
+  },
+  {
+    id: 14,
+    description: 'Rent',
+    amount: -1000.0,
+    date: '2023-11-14',
+    category: 'Housing',
+    type: 'expense',
+  },
+  {
+    id: 15,
+    description: 'New Laptop',
+    amount: -1200.0,
+    date: '2023-11-15',
+    category: 'Shopping',
+    type: 'expense',
+  },
+  {
+    id: 16,
+    description: 'Salary',
+    amount: 2000.0,
+    date: '2023-13-02',
+    category: 'income',
+    type: 'income',
+  },
+  {
+    id: 17,
+    description: 'Salary',
+    amount: 2000.0,
+    date: '2023-14-02',
+    category: 'income',
+    type: 'income',
+  },
+  {
+    id: 18,
+    description: 'Salary',
+    amount: 2000.0,
+    date: '2023-15-02',
+    category: 'income',
+    type: 'income',
+  },
+]
